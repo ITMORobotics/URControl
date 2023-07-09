910856af13864e2a8f1122cf8a70d37fbc2160a3
Vlad1slav <vvlad1slavv@yandex.ru>
2023-07-09 14:51:00 +0300

    Fix robot.update_state

--- examples/ur5e_up_down_move.py
+++ examples/ur5e_up_down_move.py
@@ -11,22 +11,23 @@ np.set_printoptions(precision=4, suppress=True)
 J_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
 
 BASE = 'base_link'
-TOOL = 'tool0'
+TOOL = 'camera_link'
 NUM_JOINTS = 6
 
 CONTROL_LOOP_DT = 1.0/100 # 100 hz
 ACCELERATION = 0.5
 
-JOINTS  = np.array([np.pi/2, -np.pi/2 + np.pi/6 - 0.2, -np.pi/2, -np.pi/2 - np.pi/6 + 0.2, np.pi/2, 0])
+JOINTS = np.array([np.pi/2, -np.pi/2 + np.pi/6 - 0.2, -np.pi/2, -np.pi/2 - np.pi/6 + 0.2, np.pi/2, 0])
 print(JOINTS)
 
 median = md.MedianFilter(NUM_JOINTS, 16)
 
 robot_ip = os.getenv('ROBOT_IP', '192.168.88.6')
 robot= ur_control.UniversalRobot(robot_ip)
-robot_model = ur_control.RobotModel('ur5e_fc.urdf', BASE, TOOL)
+robot_model = ur_control.RobotModel('ur5e_pybullet.urdf', BASE, TOOL)
 
 def ee_local_velocity_2_joint_velocity(ee_vel: np.ndarray):
+    robot.update_state()
     ee_base_bel = np.kron(
             np.eye(2),
             robot_model.rot(robot.state.q)
